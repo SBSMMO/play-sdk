287f28bb2f0649739c7c7c405a21560924acebcc
Nick Shaw <nick.shaw@g4me.co.uk>
2016-06-12 15:03:40 +0100

    removing default tests

--- common/src/main/scala/com/example/Hello.scala
+++
@@ -1,9 +0,0 @@
-package com.example
-
-object Hello {
-  
-  def sayHello(): String = {
-    "Hello, world!"
-  }
-  
-}
--- common/src/test/scala/HelloSpec.scala
+++
@@ -1,9 +0,0 @@
-import org.scalatest._
-import com.example.Hello
-
-class HelloSpec extends FlatSpec with Matchers {
-
-  "Object Hello" should "return correct greeting" in {
-    Hello.sayHello == "Hello, world!"
-  }
-}
